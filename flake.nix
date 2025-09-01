189c2033f6cd44e640f160a136dbdc5a3f363a76
Slendi <slendi@socopon.com>
2025-09-01 22:54:12 +0300

    Package for nix
    Signed-off-by: Slendi <slendi@socopon.com>

---
+++ flake.nix
@@ -0,0 +1,107 @@
+{
+  description = "revolutionary new technology that turns any image into obama";
+
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                inputs.rust-overlay.overlays.default
+                inputs.self.overlays.default
+              ];
+            };
+            systemStr = system;
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: {
+        rustToolchain =
+          let
+            rust = prev.rust-bin;
+          in
+          if builtins.pathExists ./rust-toolchain.toml then
+            rust.fromRustupToolchainFile ./rust-toolchain.toml
+          else if builtins.pathExists ./rust-toolchain then
+            rust.fromRustupToolchainFile ./rust-toolchain
+          else
+            rust.stable.latest.default.override {
+              extensions = [
+                "rust-src"
+                "rustfmt"
+              ];
+            };
+      };
+
+      packages = forEachSupportedSystem (
+        { pkgs, systemStr }:
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "obamify";
+            version = "0.1.0";
+
+            src = ./.;
+
+            cargoLock = {
+              lockFile = ./Cargo.lock;
+            };
+
+            nativeBuildInputs = [ pkgs.pkg-config ];
+            buildInputs = [ pkgs.openssl ];
+          };
+        }
+      );
+
+      apps = forEachSupportedSystem (
+        { pkgs, systemStr }:
+        {
+          default = {
+            type = "app";
+            program = "${inputs.self.packages.${systemStr}.default}/bin/obamify";
+          };
+        }
+      );
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustToolchain
+              openssl
+              pkg-config
+              cargo-deny
+              cargo-edit
+              cargo-watch
+              rust-analyzer
+            ];
+
+            env = {
+              # Required by rust-analyzer
+              RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+            };
+          };
+        }
+      );
+    };
+}
